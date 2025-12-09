b68664c2d922fb78849040368cccc44d0477edbe
DARK-ERRO <najmakhtun337@gmail.com>
2025-12-09 14:46:27 +0530

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,21 @@
+export const mainMenu = `
+💥 *GudMarani_X_Bot Main Menu* 💥
+
+🔥 /menu
+🔥 /help
+🔥 /owner
+🔥 /alive
+🔥 /ping
+
+📁 *Categories*
+✨ /fun
+✨ /tools
+✨ /downloader
+✨ /ai
+✨ /utility
+✨ /nsfw (lock)
+✨ /dev
+✨ /extra350
+
+Type any command to use.
+`;
