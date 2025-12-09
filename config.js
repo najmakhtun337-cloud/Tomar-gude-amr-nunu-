a283db61637d0b4bf6b96814f75e612831f5c1aa
DARK-ERRO <najmakhtun337@gmail.com>
2025-12-09 14:45:53 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+export const BOT_TOKEN = "8562315988:AAGrjtwzjkpI1ImKZOwkLAdrifhD62C1hUQ";
+
+export const OWNER = {
+  name: "CK-ا ERROR",
+  number: "+919046579718",
+  photo: "https://files.catbox.moe/tpqa1r.jpg"
+};
